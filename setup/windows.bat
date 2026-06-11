358dd26c284863f76bc9d2f79af0f1474c3747ad
SuperHero2010 <Imjoohn2@gmail.com>
2026-06-11 13:25:33 +0700

    Change to latest grig.audio

--- setup/windows.bat
+++ setup/windows.bat
@@ -21,7 +21,7 @@ haxelib set openfl 9.3.3
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
+haxelib install grig.audio
 haxelib git away3d https://github.com/moxie-coder/away3d
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
 echo Finished!
