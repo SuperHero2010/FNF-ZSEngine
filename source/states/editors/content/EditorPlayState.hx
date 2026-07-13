e5dfd014fd3f630fbfdc0c5984a2bb86cb204925
SuperHero2010 <Imjoohn2@gmail.com>
2026-07-13 15:25:25 +0700

    Update Tip text

--- source/states/editors/content/EditorPlayState.hx
+++ source/states/editors/content/EditorPlayState.hx
@@ -144,7 +144,7 @@ class EditorPlayState extends MusicBeatSubstate
 		dataTxt.borderSize = 1.25;
 		add(dataTxt);
 
-		var tipText:FlxText = new FlxText(10, FlxG.height - 24, 0, 'Press ESC to Go Back to Chart Editor', 16);
+		var tipText:FlxText = new FlxText(10, FlxG.height - 24, 0, 'Press 6 to turn on/off Botplay Mode\nPress ESC to Go Back to Chart Editor', 16);
 		tipText.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		tipText.borderSize = 2;
 		tipText.scrollFactor.set();
