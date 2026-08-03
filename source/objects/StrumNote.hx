1ceffb017dec6d3496701e0616e1121bc47e52e8
SuperHero2010 <Imjoohn2@gmail.com>
2026-08-03 10:29:05 +0700

    Add noteShaders to StrumNote

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -30,7 +30,7 @@ class StrumNote extends FlxSprite
 
 		rgbShader = new RGBShaderReference(this, Note.initializeGlobalRGBShader(leData));
 		rgbShader.enabled = false;
-		if(PlayState.SONG != null && PlayState.SONG.disableNoteRGB) useRGBShader = false;
+		if(PlayState.SONG != null && PlayState.SONG.disableNoteRGB || !ClientPrefs.data.noteShaders) useRGBShader = false;
 
 		var arr:Array<FlxColor> = ClientPrefs.data.arrowRGB[leData];
 		if(PlayState.isPixelStage) arr = ClientPrefs.data.arrowRGBPixel[leData];
