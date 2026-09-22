63846bd437883524b31f61e212d9bb24dc3cdcf5
SuperHero2010 <Imjoohn2@gmail.com>
2026-09-22 17:22:57 +0700

    Add `?` to unused parameters

--- source/states/editors/NoteRGBExporterState.hx
+++ source/states/editors/NoteRGBExporterState.hx
@@ -446,7 +446,7 @@ class NoteRGBExporterState extends MusicBeatState
         }
     }
 
-    function refreshUI(_name:String, _value:String)
+    function refreshUI(?name:String, ?value:String)
     {
         for (i in 0...4) {
             var tempNote = new Note(0, i);
